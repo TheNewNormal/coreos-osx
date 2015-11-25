d9ee7312dbb411f81b0f44eead93550c6f870d13
Rimantas Mocevicius <rmocius@gmail.com>
2015-11-25 19:59:44 +0000

    v1.0.8

--- src/functions.sh
+++ src/functions.sh
@@ -221,9 +221,32 @@ echo " "
 echo "This is not the password to access VM via ssh or console !!!"
 echo " "
 echo "Please type your Mac user's password followed by [ENTER]:"
-read -s password
-security add-generic-password -a coreos-osx-app -s coreos-osx-app -w $password -U
-echo " "
+read -s my_password
+passwd_ok=0
+
+# check if sudo password is correct
+while [ ! $passwd_ok = 1 ]
+do
+    # reset sudo
+    sudo -k
+    # check sudo
+    echo -e "$my_password\n" | sudo -Sv > /dev/null 2>&1
+    CAN_I_RUN_SUDO=$(sudo -n uptime 2>&1|grep "load"|wc -l)
+    if [ ${CAN_I_RUN_SUDO} -gt 0 ]
+    then
+        echo "The sudo password is fine !!!"
+        echo " "
+        passwd_ok=1
+    else
+        echo " "
+        echo "The password you entered does not match your Mac user password !!!"
+        echo "Please type your Mac user's password followed by [ENTER]:"
+        read -s my_password
+    fi
+done
+
+security add-generic-password -a coreos-osx-app -s coreos-osx-app -w $my_password -U
+
 }
 
 
